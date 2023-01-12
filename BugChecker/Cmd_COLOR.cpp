32ea35c167aba6965650e73f5e8b1b6a88683c07
Luca D'Amico <damico.luca91@live.it>
2023-01-12 22:19:00 +0100

    COLOR command: implemented RESET parameter,  optimized color vector access

--- BugChecker/Cmd_COLOR.cpp
+++ BugChecker/Cmd_COLOR.cpp
@@ -8,8 +8,8 @@ class Cmd_COLOR : public Cmd
 public:
 
 	virtual const CHAR* GetId() { return "COLOR"; }
-	virtual const CHAR* GetDesc() { return "Display or set the screen colors"; }
-	virtual const CHAR* GetSyntax() { return "COLOR [normal bold reverse help line]"; }
+	virtual const CHAR* GetDesc() { return "Display, set or reset the screen colors"; }
+	virtual const CHAR* GetSyntax() { return "COLOR [normal bold reverse help line]|[reset]"; }
 
 	virtual BcCoroutine Execute(CmdParams& params) noexcept
 	{
@@ -24,17 +24,30 @@ public:
 		}
 		else if (args.size() == 2)
 		{
-			auto colors = ParseListOfBytesArgs("COLOR", params.cmd, 6);
+			eastl::vector<BYTE> colors;
+			if (args[1] == "reset" || args[1] == "RESET")
+			{
+				colors.reserve(5);
+				colors.push_back(0x07);
+				colors.push_back(0x70);
+				colors.push_back(0x71);
+				colors.push_back(0x30);
+				colors.push_back(0x02);
+			}
+			else
+			{
+				colors = ParseListOfBytesArgs("COLOR", params.cmd, 6);
+			}
 			if (colors.size() != 5)
 			{
 				Print("Arguments number mismatch.");
 				co_return;
 			}
-			Wnd::nrmClr = colors[0];
-			Wnd::bldClr = colors[1];
-			Wnd::rvrClr = colors[2];
-			Wnd::hlpClr = colors[3];
-			Wnd::hrzClr = colors[4];
+			Wnd::nrmClr = colors.at(0);
+			Wnd::bldClr = colors.at(1);
+			Wnd::rvrClr = colors.at(2);
+			Wnd::hlpClr = colors.at(3);
+			Wnd::hrzClr = colors.at(4);
 			params.result = CmdParamsResult::RedrawUi;
 			co_return;
 		}
